8f640a5aa5e2f171d9efdfd55468b4f671cf7b9d
MY PC <MY PC@HOME-UEFI1TB>
2018-09-26 18:14:27 +0700

    Project GandyCSitumorang 10116228 PBO6

---
+++ src/pbo6/pkg10116228/latihan1/biodata/PBO610116228Latihan1Biodata.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6.pkg10116228.latihan1.biodata;
+
+/**
+ *
+ * @author Rizki Adam Kurniawan
+ */
+public class PBO610116228Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Biodata" + "\n" + "--------------------------");
+        System.out.println("Nama\t\t: Gandy Christian Situmorang");
+        System.out.println("Alamat\t\t: Jl. Atletik 9 No.11");
+        System.out.println("Tempat Lahir\t: Pontianak");
+        System.out.println("Tanggal lahir\t: 22 Agustus 1995");
+        System.out.println("-----------------------------------");
+        System.out.println("Hobi\t\t: Traveling, Naik Gunung");
+        System.out.println("MaKes\t\t: Bakso");
+        System.out.println("MiKes\t\t: Capucino");
+    }
+    
+}
